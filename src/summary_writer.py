9733bce77cb000947f2458ef209bb44deda630cd
janolockhart <janolockhart@gmail.com>
2025-06-17 16:24:07 -0300

    Added epoch number to summary and corrected json dump

--- src/summary_writer.py
+++ src/summary_writer.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass, asdict
 
 @dataclass(frozen=True)
 class EpochSummary:
+    epoch:int
     char_error_rate:float
     word_accuracies:float
     average_train_loss:float
@@ -18,4 +19,4 @@ class SummaryWriter:
     def append(self, epoch_summary: EpochSummary) -> None:
        self.summaries.append(asdict(epoch_summary))
        with open(self.path, 'w') as file:
-           json.dump({self.summaries}, file, ident=4)
+           json.dump(self.summaries, file, indent=4)
