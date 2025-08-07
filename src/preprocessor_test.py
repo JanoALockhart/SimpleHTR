ed582cfa20979be22d87a0421e34b453b507c989
janolockhart <janolockhart@gmail.com>
2025-08-06 23:55:10 -0300

    Defined _set_up() method to load a fresh image for each test and added _visualize as a method of the PreprocessorTest class

--- src/preprocessor_test.py
+++ src/preprocessor_test.py
@@ -3,25 +3,32 @@ import matplotlib.pyplot as plt
 from preprocessor import Preprocessor
 
 class PreprocessorTest():
-    def __init__(self, preprocessor):
+    def __init__(self, preprocessor, img_path = '../data/line.png'):
         self.preprocessor = preprocessor
+        self.img_path = img_path
+        self.base_image = cv2.imread(self.img_path, cv2.IMREAD_GRAYSCALE)
+
+    def _set_up(self):
+        self.original_img = cv2.imread(self.img_path, cv2.IMREAD_GRAYSCALE)
 
     def test_random_preprocess_image(self):
-        img = cv2.imread('../data/line.png', cv2.IMREAD_GRAYSCALE)
-        img_aug = self.preprocessor.process_img(img)
+        self._set_up()
+
+        img_aug = self.preprocessor.process_img(self.original_img)
         img_aug = cv2.transpose(img_aug) + 0.5
-        visualize(img, img_aug)
+        
+        self._visualize(self.original_img, img_aug)
 
-def visualize(original, augmented):
-  fig = plt.figure()
-  plt.subplot(2,1,1)
-  plt.title('Original image')
-  plt.imshow(original, cmap='gray')
+    def _visualize(self, original, augmented):
+        fig = plt.figure()
+        plt.subplot(2,1,1)
+        plt.title('Original image')
+        plt.imshow(original, cmap='gray')
 
-  plt.subplot(2,1,2)
-  plt.title('Augmented image')
-  plt.imshow(augmented, cmap='gray', vmin=0, vmax=1)
-  plt.show()
+        plt.subplot(2,1,2)
+        plt.title('Augmented image')
+        plt.imshow(augmented, cmap='gray', vmin=0, vmax=1)
+        plt.show()
 
 def main():
     preprocessor = Preprocessor((256, 32), data_augmentation=True)
