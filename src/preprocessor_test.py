f776c17dc0b7911e154507b5822a055bba1638e9
janolockhart <janolockhart@gmail.com>
2025-08-06 23:14:20 -0300

    Visualization function for image comparison

--- src/preprocessor_test.py
+++ src/preprocessor_test.py
@@ -5,11 +5,19 @@ from preprocessor import Preprocessor
 def test_random_preprocess_image():
     img = cv2.imread('../data/line.png', cv2.IMREAD_GRAYSCALE)
     img_aug = Preprocessor((256, 32), data_augmentation=True).process_img(img)
-    plt.subplot(121)
-    plt.imshow(img, cmap='gray')
-    plt.subplot(122)
-    plt.imshow(cv2.transpose(img_aug) + 0.5, cmap='gray', vmin=0, vmax=1)
-    plt.show()
+    img_aug = cv2.transpose(img_aug) + 0.5
+    visualize(img, img_aug)
+
+def visualize(original, augmented):
+  fig = plt.figure()
+  plt.subplot(2,1,1)
+  plt.title('Original image')
+  plt.imshow(original, cmap='gray')
+
+  plt.subplot(2,1,2)
+  plt.title('Augmented image')
+  plt.imshow(augmented, cmap='gray', vmin=0, vmax=1)
+  plt.show()
 
 def main():
     test_random_preprocess_image()
