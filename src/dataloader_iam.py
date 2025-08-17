3c289c950861f4e26d09d049578e30bc90744689
janolockhart <janolockhart@gmail.com>
2025-08-17 19:07:16 -0300

    Moved Dataset to dataset file

--- src/dataloader_iam.py
+++ src/dataloader_iam.py
@@ -7,8 +7,7 @@ import lmdb
 import numpy as np
 from path import Path
 
-from dataset import AbstractDataset, Batch, DatasetLoader, Sample
-from preprocessor import Preprocessor
+from dataset import AbstractDataset, Dataset, DatasetLoader, Sample
 from settings import Settings
 
 class DataLoaderIAM(DatasetLoader):
@@ -110,72 +109,3 @@ class DataLoaderIAM(DatasetLoader):
         file_name = line_split[0] + '.png'
         file_path = data_dir / 'img' / fist_subdir_name / second_subdir_name / file_name
         return file_path   
-    
-class Dataset(AbstractDataset):
-    def __init__(self, 
-                 samples:List[Sample], 
-                 batch_size:int,
-                 data_dir: Path,
-                 drop_remainder:bool = False, 
-                 shuffle:bool = False,
-                 fast: bool = True):
-        self.samples = samples
-        self.batch_size = batch_size
-        self.drop_remainder = drop_remainder
-        self.shuffle = shuffle
-        self.fast = fast
-        self.curr_idx = 0
-        self.preprocessor: Preprocessor = None
-
-        if fast:
-            self.env = lmdb.open(str(data_dir / 'lmdb'), readonly=True)
-
-    def map(self, preprocessor: Preprocessor) -> AbstractDataset:
-        self.preprocessor = preprocessor
-        return self
-
-    def has_next(self) -> bool:
-        """Is there a next element?"""
-        if self.drop_remainder:
-            return self.curr_idx + self.batch_size <= len(self.samples)  # train set: only full-sized batches
-        else:
-            return self.curr_idx < len(self.samples)  # val set: allow last batch to be smaller
-    
-    def get_iterator_info(self) -> Tuple[int, int]:
-        """Current batch index and overall number of batches."""
-        if self.drop_remainder:
-            num_batches = int(np.floor(len(self.samples) / self.batch_size))  # train set: only full-sized batches
-        else:
-            num_batches = int(np.ceil(len(self.samples) / self.batch_size))  # val set: allow last batch to be smaller
-        curr_batch = self.curr_idx // self.batch_size + 1
-        return curr_batch, num_batches
-
-    def get_next(self) -> Batch:
-        """Get next element."""
-        batch_range = range(self.curr_idx, min(self.curr_idx + self.batch_size, len(self.samples)))
-
-        imgs = [self._get_img(i) for i in batch_range]
-        gt_texts = [self.samples[i].gt_text for i in batch_range]
-
-        self.curr_idx += self.batch_size
-        batch = Batch(imgs, gt_texts, len(imgs))
-        if Preprocessor is not None:
-            self.preprocessor.process_batch(batch)
-            
-        return batch
-
-    def _get_img(self, i: int) -> np.ndarray:
-        if self.fast:
-            with self.env.begin() as txn:
-                basename = Path(self.samples[i].file_path).basename()
-                data = txn.get(basename.encode("ascii"))
-                img = pickle.loads(data)
-        else:
-            img = cv2.imread(self.samples[i].file_path, cv2.IMREAD_GRAYSCALE)
-
-        return img
-    
-    def reset_iterator(self):
-        if self.shuffle:
-            random.shuffle(self.samples)
-        self.curr_idx = 0
