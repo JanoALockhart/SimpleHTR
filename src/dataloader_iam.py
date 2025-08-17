317f8083e9d5611ec1c9f6173011948154effd59
janolockhart <janolockhart@gmail.com>
2025-08-16 23:37:32 -0300

    Renamed variables

--- src/dataloader_iam.py
+++ src/dataloader_iam.py
@@ -36,10 +36,10 @@ class DataLoaderIAM:
         self.batch_size = batch_size
         self.samples = []
 
-        f = open(data_dir / 'gt/words.txt')
+        ground_truths_file = open(data_dir / 'gt/words.txt')
         chars = set()
         bad_samples_reference = ['a01-117-05-02', 'r06-022-03-05']  # known broken images in IAM dataset
-        for line in f:
+        for line in ground_truths_file:
             # ignore empty and comment lines
             line = line.strip()
             if not line or line[0] == '#':
@@ -50,13 +50,13 @@ class DataLoaderIAM:
 
             # filename: part1-part2-part3 --> part1/part1-part2/part1-part2-part3.png
             file_name_split = line_split[0].split('-')
-            file_name_subdir1 = file_name_split[0]
-            file_name_subdir2 = f'{file_name_split[0]}-{file_name_split[1]}'
-            file_base_name = line_split[0] + '.png'
-            file_name = data_dir / 'img' / file_name_subdir1 / file_name_subdir2 / file_base_name
+            fist_subdir_name = file_name_split[0]
+            second_subdir_name = f'{file_name_split[0]}-{file_name_split[1]}'
+            file_name = line_split[0] + '.png'
+            file_path = data_dir / 'img' / fist_subdir_name / second_subdir_name / file_name
 
             if line_split[0] in bad_samples_reference:
-                print('Ignoring known broken image:', file_name)
+                print('Ignoring known broken image:', file_path)
                 continue
 
             # GT text are columns starting at 9
@@ -64,7 +64,7 @@ class DataLoaderIAM:
             chars = chars.union(set(list(gt_text)))
 
             # put sample into list
-            self.samples.append(Sample(gt_text, file_name))
+            self.samples.append(Sample(gt_text, file_path))
 
         # split into training and validation set: 95% - 5%
         split_idx = int(data_split * len(self.samples))
