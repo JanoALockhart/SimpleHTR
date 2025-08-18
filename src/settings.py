7b011da0e47f7d942c8c43aee54195a2c3ef39c5
janolockhart <janolockhart@gmail.com>
2025-08-18 17:48:46 -0300

    Removed char file path from Settings

--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,5 @@
 class Settings:
     """Filenames and paths to data."""
-    CHAR_LIST_FILE_PATH = '../model/charList.txt'
     SUMMARY_FILE_PATH = '../model/summary.json'
     CORPUS_FILE_PATH = '../data/corpus.txt'
     WORD_CHAR_LIST_FILE_PATH = '../model/wordCharList.txt'
