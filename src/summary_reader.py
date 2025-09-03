d9615be07ca50300868b27859ba8eb47af8e7f45
janolockhart <janolockhart@gmail.com>
2025-09-03 00:25:46 -0300

    Modified summary reader to plot training and validation metrics

--- src/summary_reader.py
+++ src/summary_reader.py
@@ -3,13 +3,14 @@ import matplotlib.pyplot as plt
 
 from summary_writer import EpochSummary
 
-def plot_metric(title, metric):
-    line = plt.plot(metric)
-    plt.setp(line, label=title)
+def plot_metric(title, train_metric, val_metric):
+    train_line = plt.plot(train_metric)
+    val_line = plt.plot(val_metric)
+    plt.setp(train_line, label=f"train {title}")
+    plt.setp(val_line, label=f"val {title}")
     plt.xlabel("Epoch")
     plt.legend()
     plt.savefig("../model/"+title+".png")
-    #plt.show()
     plt.close()
     
 
@@ -19,23 +20,41 @@ def plot_summary():
 
     epochs = [EpochSummary(**epoch_summary) for epoch_summary in data]
 
-    loss = [epoch_summary.train_loss for epoch_summary in epochs]
-    character_error_rate = [epoch_summary.val_cer for epoch_summary in epochs]
-    word_accuracy = [epoch_summary.val_phrase_acc for epoch_summary in epochs]
+    train_loss = [epoch_summary.train_loss for epoch_summary in epochs]
+    val_loss = [epoch_summary.val_loss for epoch_summary in epochs]
+    plot_metric("Loss", train_loss, val_loss)
+
+    train_cer = [epoch_summary.train_cer for epoch_summary in epochs]
+    val_cer = [epoch_summary.val_cer for epoch_summary in epochs]
+    plot_metric("CER", train_cer, val_cer)
     
-    plot_metric("Loss", loss)
-    plot_metric("CER", character_error_rate)
-    plot_metric("Phrase Accuracy", word_accuracy)
+    train_wer = [epoch_summary.train_wer for epoch_summary in epochs]
+    val_wer = [epoch_summary.val_wer for epoch_summary in epochs]
+    plot_metric("WER", train_wer, val_wer)
+
+    train_phrase_acc = [epoch_summary.train_phrase_acc for epoch_summary in epochs]
+    val_phrase_acc = [epoch_summary.val_phrase_acc for epoch_summary in epochs]
+    plot_metric("Phrase Accuracy", train_phrase_acc, val_phrase_acc)
 
     total_time_to_train = sum([epoch_summary.time_to_train_epoch for epoch_summary in epochs])
-    saved_model_cer = min(character_error_rate)
-    saved_model_word_accuracy = word_accuracy[character_error_rate.index(saved_model_cer)]
+    
+    saved_model_val_cer = min(val_cer)
+    epoch_saved = val_cer.index(saved_model_val_cer)
+    saved_model_train_cer = train_cer[epoch_saved]
+    saved_model_train_wer = train_wer[epoch_saved]
+    saved_model_val_wer = val_wer[epoch_saved]
+    saved_model_train_phrase_acc = train_phrase_acc[epoch_saved]
+    saved_model_val_phrase_acc = val_phrase_acc[epoch_saved]
 
     with open("../model/description.txt", 'w') as file:
         file.write(f"Total Epochs: {len(epochs)} \n")
-        file.write(f"Time to train (hours): {total_time_to_train / 3600: .2f} \n")
-        file.write(f"CER: {saved_model_cer*100: .2f}% \n")
-        file.write(f"Phrase Accuracy: {saved_model_word_accuracy*100: .2f}% \n")
+        file.write(f"Time to train: {total_time_to_train / 3600: .2f}hs \n")
+        file.write(f"Train CER: {saved_model_train_cer*100: .2f}% \n")
+        file.write(f"Validation CER: {saved_model_val_cer*100: .2f}% \n")
+        file.write(f"Train WER: {saved_model_train_wer*100: .2f}% \n")
+        file.write(f"Validation WER: {saved_model_val_wer*100: .2f}% \n")
+        file.write(f"Train Phrase Accuracy: {saved_model_train_phrase_acc*100: .2f}% \n")
+        file.write(f"Validation Phrase Accuracy: {saved_model_val_phrase_acc*100: .2f}% \n")
 
 if __name__ == '__main__':
     plot_summary()
